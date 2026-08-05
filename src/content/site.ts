86d72c4799868fedba08508984607f712acc5c4c
Luca Furtado <lucafurtadomkt@gmail.com>
2026-08-05 16:37:46 -0300

    Fix LinkedIn profile URL

--- src/content/site.ts
+++ src/content/site.ts
@@ -1,5 +1,5 @@
 export const site = {
   github: "https://github.com/lucafurtado",
   email: "luca.bernastudio@gmail.com",
-  linkedin: "https://www.linkedin.com/in/luca-furtado-b7a8b7181/",
+  linkedin: "https://www.linkedin.com/in/lucafurtado/",
 };
